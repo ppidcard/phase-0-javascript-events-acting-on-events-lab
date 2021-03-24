66765c82ee685188ea3cde998455032ba3f6fd14
ppidcard <tvxqlikei@live.cn>
2021-03-24 16:36:29 +1100

    done with assigment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+
+const dodger = document.getElementById("dodger");
+
+
+function moveDodgerLeft() {
+    const left_ = dodger.style.left.replace("px", "");
+    const left = parseInt(left_, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+function moveDodgerRight() {
+    const left_ = dodger.style.left.replace("px", "");
+    const left = parseInt(left_, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+
+  document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  }); 
